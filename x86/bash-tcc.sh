dd62c161fc6551f8fb4997876b64310161831786
fosslinux <fosslinux@aussies.space>
2020-03-24 14:22:22 +1100

    bash-tcc: Copy required files

--- x86/bash-tcc.sh
+++ x86/bash-tcc.sh
@@ -13,6 +13,7 @@ export CFLAGS="-D _POSIX_VERSION=1"
 cd ../sources/bash-tcc
 cp ../tinycc-mes/crt*.o .
 cp ../tinycc-mes/crt*.o support/ 
+cp ../tinycc-mes/crt*.o builtins/ 
 cp ../tinycc-mes/libgetopt.a .
 
 ${REPO}/sources/gash-assets/gash configure --build=i686-unknown-linux-gnu --host=i686-unknown-linux-gnu --without-bash-malloc --disable-readline --disable-history --disable-help-builtin --disable-progcomp --disable-net-redirections --disable-nls ac_cv_func_dlopen=no
